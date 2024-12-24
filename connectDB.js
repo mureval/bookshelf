6e2e96ab3a5bf1ff6aad3877524cb75142a6e3e0
mureval <muh.revan0801@gmail.com>
2024-12-24 16:01:04 +0700

    update code

--- connectDB.js
+++ connectDB.js
@@ -1,11 +1,14 @@
 import pg from "pg";
+import env from "dotenv";
+
+env.config();
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "bookshelf",
-  password: "rev081",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD_DB,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
